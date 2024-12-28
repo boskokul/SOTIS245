18b410a52e48d28479f9e43495c12a63ea13dfe5
Bosko Kulusic <kulusicbosko@gmail.com>
2024-12-28 01:33:36 +0100

    some code refactoring

--- SOTISProj/Controllers/PythonController.cs
+++ SOTISProj/Controllers/PythonController.cs
@@ -17,244 +17,16 @@ namespace SOTISProj.Controllers
         private readonly IDataService _dataService;
         private readonly MyDbContext _context;
         private readonly ITestService _testService;
+        private readonly string pathJelena = "C:\\Users\\Laptop\\anaconda3\\envs\\SOTIS\\python.exe";
+        private readonly string pathBosko = "C:\\Users\\bosko\\Desktop\\SOTIS\\okruzenje\\Scripts\\python.exe";
+
         public PythonController(IDataService dataService, MyDbContext context, ITestService testService)
         {
             _dataService = dataService;
             _context = context;
             _testService = testService;
         }
-        [HttpGet("parsePDF")]
-        public IActionResult RunParse()
-        {
-            var scriptPath = "..\\SOTISProj\\PythonScripts\\parsePDF.py";
-            var pdfPath = "..\\SOTISProj\\PDFs\\networks2.pdf";
-            var start = new ProcessStartInfo
-            {
-                FileName = "C:\\Users\\bosko\\Desktop\\SOTIS\\okruzenje\\Scripts\\python.exe", 
-                Arguments = $"{scriptPath} {pdfPath}",
-                RedirectStandardOutput = true,
-                RedirectStandardError = true, // Capture standard error
-                UseShellExecute = false,
-                CreateNoWindow = true
-            };
-            string errors;
-            string ParsedText;
-            try
-            {
-                using (var process = Process.Start(start))
-                {
-                    using (var reader = process.StandardOutput)
-                    {
-                        ParsedText = reader.ReadToEnd();
-                    }
-                    errors = process.StandardError.ReadToEnd();
-                    if (!string.IsNullOrWhiteSpace(ParsedText))
-                    {
-                        Debug.WriteLine($"Standard Output: {ParsedText}");
-                        _dataService.SavePDFContent(ParsedText);
-                    }
-                    if (!string.IsNullOrWhiteSpace(errors))
-                    {
-                        Debug.WriteLine($"Standard Error: {errors}");
-                    }
-                }
-
-                if (!string.IsNullOrWhiteSpace(errors))
-                {
-                    return BadRequest($"Error running Python script: {errors}");
-                }
-
-                return Ok(ParsedText);
-            }
-            catch (Exception ex)
-            {
-                return BadRequest($"An error occurred: {ex.Message}");
-            }
-        }
-
-        
-        [HttpGet("ExtractTerms")]
-        public IActionResult RunTermsExraction()
-        {
-            var scriptPath = "..\\SOTISProj\\PythonScripts\\first.py";
-            var start = new ProcessStartInfo
-            {
-                FileName = "C:\\Users\\bosko\\Desktop\\SOTIS\\okruzenje\\Scripts\\python.exe",
-                Arguments = $"{scriptPath} \"{_dataService.GetPDFContent()}\" \"{_dataService.GetAcmSubTree()}\"",
-                RedirectStandardOutput = true,
-                RedirectStandardError = true,
-                UseShellExecute = false,
-                CreateNoWindow = true
-            };
-
-            string errors;
-            string res;
-            try
-            {
-                using (var process = Process.Start(start))
-                {
-                    using (var reader = process.StandardOutput)
-                    {
-                        res = reader.ReadToEnd();
-                    }
-                    errors = process.StandardError.ReadToEnd();
-                    if (!string.IsNullOrWhiteSpace(res))
-                    {
-                        Debug.WriteLine($"Standard Output: {res}");
-                        string jsonString1 = res.Split("###")[0];
-                        string jsonString2 = res.Split("###")[1];
-
-                        jsonString1 = jsonString1.Replace('\'', '"');
-
-                        Dictionary<string, string> definitions = JsonSerializer.Deserialize<Dictionary<string, string>>(jsonString1);
-                        _dataService.SaveTermsDefinitions(definitions);
-
-                        foreach (var item in definitions)
-                        {
-                            Console.WriteLine($"{item.Key}: {item.Value}\n");
-                        }
-
-                        jsonString2 = jsonString2.Replace('\'', '"');
-
-                        var relations = JsonSerializer.Deserialize<Dictionary<string, RelatedTerms>>(jsonString2, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
-
-                        _dataService.SaveTermsRelations(relations);
-
-                        foreach (var item in relations)
-                        {
-                            Console.WriteLine($"{item.Key}:");
-                            foreach (var relatedTerm in item.Value.Related_to)
-                            {
-                                Console.WriteLine($"  - {relatedTerm}");
-                            }
-                            Console.WriteLine();
-                        }
-                    }
-                    if (!string.IsNullOrWhiteSpace(errors))
-                    {
-                        Debug.WriteLine($"Standard Error: {errors}");
-                    }
-                }
-
-                if (!string.IsNullOrWhiteSpace(errors))
-                {
-                    return BadRequest($"Error running Python script: {errors}");
-                }
-
-                return Ok(res);
-            }
-            catch (Exception ex)
-            {
-                return BadRequest($"An error occurred: {ex.Message}");
-            }
-        }
-
-        [HttpGet("FindNewRelations")]
-        public IActionResult RunTermsDefinitions()
-        {
-            string terms = "";
-            foreach(var t  in _dataService.GetTermsRelations())
-            {
-                terms += t.Key + ", ";
-            }
-            var scriptPath = "..\\SOTISProj\\PythonScripts\\second.py";
-            var start = new ProcessStartInfo
-            {
-                FileName = "C:\\Users\\bosko\\Desktop\\SOTIS\\okruzenje\\Scripts\\python.exe",
-                Arguments = $"{scriptPath} \"{terms}\" \"{_dataService.GetAcmSubTree()}\" \"{_dataService.GetTermsRelations()}\"",
-                RedirectStandardOutput = true,
-                RedirectStandardError = true,
-                UseShellExecute = false,
-                CreateNoWindow = true
-            };
-            string errors;
-            string TermsJSON;
-            try
-            {
-                using (var process = Process.Start(start))
-                {
-                    using (var reader = process.StandardOutput)
-                    {
-                        TermsJSON = reader.ReadToEnd();
-                    }
-                    errors = process.StandardError.ReadToEnd();
-
-                    if (!string.IsNullOrWhiteSpace(TermsJSON))
-                    {
-                        Debug.WriteLine($"Standard Output: {TermsJSON}");
-                    }
-                    if (!string.IsNullOrWhiteSpace(errors))
-                    {
-                        Debug.WriteLine($"Standard Error: {errors}");
-                    }
-                }
-
-                if (!string.IsNullOrWhiteSpace(errors))
-                {
-                    return BadRequest($"Error running Python script: {errors}");
-                }
-
-                return Ok(TermsJSON);
-            }
-            catch (Exception ex)
-            {
-                return BadRequest($"An error occurred: {ex.Message}");
-            }
-        }
-
-        [HttpGet("AddRelations")]
-        public IActionResult RunTermsRelationsAdding()
-        {
-            string terms = "";
-            foreach (var t in _dataService.GetTermsRelations())
-            {
-                terms += t.Key + ", ";
-            }
-            var scriptPath = "..\\SOTISProj\\PythonScripts\\third.py";
-            var start = new ProcessStartInfo
-            {
-                FileName = "C:\\Users\\bosko\\Desktop\\SOTIS\\okruzenje\\Scripts\\python.exe",
-                Arguments = $"{scriptPath} \"{terms}\" \"{_dataService.GetTermsRelationsPairs()}\"  \"{_dataService.GetTermsDefinitionsPairs()}\"",
-                RedirectStandardOutput = true,
-                RedirectStandardError = true,
-                UseShellExecute = false,
-                CreateNoWindow = true
-            };
-            string errors;
-            string TermsJSON;
-            try
-            {
-                using (var process = Process.Start(start))
-                {
-                    using (var reader = process.StandardOutput)
-                    {
-                        TermsJSON = reader.ReadToEnd();
-                    }
-                    errors = process.StandardError.ReadToEnd();
-
-                    if (!string.IsNullOrWhiteSpace(TermsJSON))
-                    {
-                        Debug.WriteLine($"Standard Output: {TermsJSON}");
-                    }
-                    if (!string.IsNullOrWhiteSpace(errors))
-                    {
-                        Debug.WriteLine($"Standard Error: {errors}");
-                    }
-                }
-
-                if (!string.IsNullOrWhiteSpace(errors))
-                {
-                    return BadRequest($"Error running Python script: {errors}");
-                }
-
-                return Ok(TermsJSON);
-            }
-            catch (Exception ex)
-            {
-                return BadRequest($"An error occurred: {ex.Message}");
-            }
-        }
-
+                
         [HttpGet("acmPart")]
         public IActionResult GetACM()
         {
@@ -304,14 +76,6 @@ namespace SOTISProj.Controllers
             {
                 return BadRequest($"An error occurred: {ex.Message}");
             }
-        }
-        [HttpPost("subject")] 
-        public IActionResult PostMyModel(Field myModel) 
-        { 
-            _context.Fields.Add(myModel); 
-            _context.SaveChanges(); 
-            return Ok(new { id = myModel.Id, name = myModel.Name });
-
         }
 
         [HttpPost("uploadPDF/{rootTerm}")]
@@ -556,7 +320,7 @@ namespace SOTISProj.Controllers
         {
             var start = new ProcessStartInfo
             {
-                FileName = "C:\\Users\\bosko\\Desktop\\SOTIS\\okruzenje\\Scripts\\python.exe",
+                FileName = pathBosko,
                 Arguments = $"{scriptPath} {arguments}",
                 RedirectStandardOutput = true,
                 RedirectStandardError = true,
